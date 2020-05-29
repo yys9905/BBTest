a34757e38b25a773f7d7819b8e1a7c979ca5c9ea
YangYousuk <yys9905a@gmail.com>
2020-05-29 10:59:39 +0900

    make method

---
+++ ABCAquarium.java
@@ -0,0 +1,13 @@
+
+/**
+ * 여기에 ABCAquarium 클래스 설명을 작성하십시오.
+ * 
+ * @author (2018315036 양유석,2018315003 최세원, 2018315022 이지연) 
+ * @version (2020.05.29)
+ */
+public class ABCAquarium
+{
+    public int pay_processing(){
+        return 0;
+    }
+}

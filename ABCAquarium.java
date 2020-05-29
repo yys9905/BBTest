0a3c50e46de6f9ae0ac63ed3976558a2fd505e2b
YangYousuk <yys9905a@gmail.com>
2020-05-29 11:35:21 +0900

    add definition

--- ABCAquarium.java
+++ ABCAquarium.java
@@ -7,21 +7,26 @@
  */
 public class ABCAquarium
 {
+    /**
+     * pay_processing - 수족관 입장료를 계산하는 메소드
+     * 
+     * @param age  입장료를 계산할 나이
+     * @return    16세 이상 2000 / 7세 이상 900 / 4세 이상 400 / 4세 미만 0
+     */
     public int pay_processing(int age){
         if (age >= 16)
         {
             return  2000;
         }
-        else if (age >= 7)
+        else if (age > 7)
         {
             return  900;
         }
         else if (age >= 4){
             return 400;
         }
-        else if (age < 4){
+        else{
             return 0;
         }
-        return 0;
     }
 }
